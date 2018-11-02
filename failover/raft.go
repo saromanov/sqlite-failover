278d8505265e2859a30ae9e2535b03a097e84940
Sergey <xxsmotur@gmail.com>
2018-11-02 21:14:25 +0500

    Add more attributes

--- failover/raft.go
+++ failover/raft.go
@@ -2,9 +2,17 @@ package failover
 
 import (
 	"github.com/hashicorp/raft"
+	"github.com/hashicorp/raft-boltdb"
 )
 
 // Failover defines main structure
 type Failover struct {
-	raft *raft.Raft
+	raft     *raft.Raft
+	dbStore  *raftboltdb.BoltStore
+	raftAddr string
+}
+
+// New creates a new failover
+func New() *Failover {
+	return &Failover{}
 }
