de28415955579e487b7432715622fb250ef566db
Sergey <xxsmotur@gmail.com>
2018-11-07 00:25:25 +0500

    Starting of the cluster

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/saromanov/sqlite-failover/failover"
+)
+
+func main() {
+	cfg := &failover.Config{
+		RaftDir:    "$HOME/raftdir",
+		RaftDBPath: "$HOME/raftdbdir",
+		RaftAddr:   "127.0.0.1:9000",
+		LocalID:    "machine-1",
+	}
+	f, err := failover.New(cfg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(f.Run())
 }
