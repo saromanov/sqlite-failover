973d0e0342ced17a4f7441b7a1215279be215ff7
Sergey <xxsmotur@gmail.com>
2018-11-11 01:46:44 +0500

    Add arguments for join to the cluster

--- main.go
+++ main.go
@@ -10,11 +10,15 @@ import (
 var (
 	addr     *string
 	raftAddr *string
+	// join defines list of the addresses 
+	// for joining to the cluster
+	join *string
 )
 
 func parse() {
 	addr = flag.String("addr", "Address", "a string")
 	raftAddr = flag.String("raft-addr", "Raft Address", "a string")
+	join = flag.String("join", "List of the addrsses for join to the cluster", "addr")
 	flag.Parse()
 }
 func main() {
