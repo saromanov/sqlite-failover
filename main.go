ed8298b2e2b275114eead90d025d3492a6104dae
Sergey <xxsmotur@gmail.com>
2018-11-11 01:35:59 +0500

    Add parsing of command line arguments

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/saromanov/sqlite-failover/failover"
 )
 
+var (
+	addr     *string
+	raftAddr *string
+)
+
+func parse() {
+	addr = flag.String("addr", "Address", "a string")
+	raftAddr = flag.String("raft-addr", "Raft Address", "a string")
+	flag.Parse()
+}
 func main() {
+	parse()
+	fmt.Println(*addr)
 	cfg := &failover.Config{
 		RaftDir:    "$HOME/raftdir",
 		RaftDBPath: "$HOME/raftdbdir",
-		RaftAddr:   "127.0.0.1:9000",
+		RaftAddr:   *raftAddr,
 		LocalID:    "machine-1",
+		Addr:       *addr,
 	}
 	f := failover.New(cfg)
 	f.Start()
